b9938164034f04ef7eb862c73dfa59560eef6f1d
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-13 07:52:55 -0500

    fixed binding style attr with height/width

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,6 +7,12 @@ export default Ember.Controller.extend({
     return new Ember.Handlebars.SafeString("color: " + color);
   }),
 
+  imageSize: Ember.computed('width', 'height', function() {
+    var width = this.get('width');
+    var height = this.get('height');
+    return new Ember.Handlebars.SafeString("width: " + width + "px;" + "height: " + height + "px;");
+  }),
+
   actions: {
     bandDidChange: function(value){
       if (value.length < 1) {
